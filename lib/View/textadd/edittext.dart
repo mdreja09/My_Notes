67ff59e71d46677e8f70e46807b62767485150ba
Reja <almialsama@gmail.com>
2025-11-26 01:18:52 +0600

    edit text

--- lib/View/textadd/edittext.dart
+++ lib/View/textadd/edittext.dart
@@ -12,42 +12,48 @@ class _EdittextState extends State<Edittext> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("Edit Text")),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Title",
-                hintStyle: TextStyle(fontSize: 25, color: Colors.grey),
-                //contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-                filled: true,
-                fillColor: Colors.white,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  hintStyle: TextStyle(fontSize: 25, color: Colors.grey),
+                  //contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                hintText: "Details",
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                maxLines: 100,
+                decoration: InputDecoration(
 
-                hintStyle: TextStyle(fontSize: 23),
-                fillColor: Colors.white,
-                filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: "Details",
+        
+                  hintStyle: TextStyle(fontSize: 23),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
